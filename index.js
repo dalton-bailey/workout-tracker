6e8c8d0e5ad626b95b1ba3b49cec525a1493e21e
Bailey Dalton <baidalton@gmail.com>
2020-11-03 22:58:38 -0700

    started building frontend/layout

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 
+const port = process.env.PORT || 3000
+
 const app = express()
 
-const port = process.env.PORT || 3000
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.listen(port, () => {
     console.log("server running on port 3000")
