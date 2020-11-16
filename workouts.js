ce6b15cc32f2ddff5de7f81acc5b5b6a0e9bee4c
Bailey Dalton <baidalton@gmail.com>
2020-11-15 20:54:38 -0700

    goalsApi

--- workouts.js
+++ workouts.js
@@ -1,5 +1,5 @@
 const api = "https://gentle-spire-21312.herokuapp.com/workouts";
-// const goalsApi = "https://gentle-spire-21312.herokuapp.com/goals"
+const goalsApi = "https://gentle-spire-21312.herokuapp.com/goals"
 // const api = "http://localhost:3000/workouts";
 let workouts = [];
 let goals = []
@@ -16,7 +16,7 @@ async function fetchWorkouts() {
 
 //fetch goals 
 async function fetchGoals() {
-  let response = await fetch(api + "/goals")
+  let response = await fetch(goalsApi)
   let fetchedGoals = await response.json()
 
   console.log(fetchedGoals)
@@ -37,7 +37,7 @@ async function postWorkout(data) {
 
 //post goal
 async function postGoal(data) {
-  let response = await fetch(api + "/goals", {
+  let response = await fetch(goalsApi, {
     method: "POST",
     body: JSON.stringify(data),
     headers: {
